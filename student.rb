8847c75dcf20060827ca2cf7ee670fd5cf09ea7a
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-05-15 17:27:57 -0500

    Add method add_classroom to student class

--- student.rb
+++ student.rb
@@ -1,14 +1,21 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :classroom
+
   # Constuctor
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, classroom, name = 'Unknown')
+    super(age, name)
     @classroom = classroom
   end
 
   # Methods
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
