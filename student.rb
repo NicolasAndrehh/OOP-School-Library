37e2f4425f7267c7c980802cfc8791459dd6ee61
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-05-15 12:29:17 -0500

    Fix linters errors

--- student.rb
+++ student.rb
@@ -1,14 +1,14 @@
 require_relative 'person'
 
 class Student < Person
-    # Constuctor
-    def initialize (age, classroom, name = "Unknown", parent_permission = true)
-        super(age, name, parent_permission)
-        @classroom = classroom
-    end
+  # Constuctor
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
 
-    #Methods
-    def play_hooky 
-        '¯\(ツ)/¯'
-    end
+  # Methods
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
