c800f3847f791c29fab239a3ee2f8b591e93e703
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-05-22 21:28:03 +0300

    - New class for managing books in the application

---
+++ book_manager.rb
@@ -0,0 +1,37 @@
+require_relative 'book'
+require_relative 'base_manager'
+
+class BookManager < BaseManager
+  attr_accessor :books
+
+  def initialize
+    @books = []
+  end
+
+  # List all books
+  def list_all_books
+    check_empty_list(@books, 'books')
+
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+    end
+  end
+
+  # Create a book
+  def create_book
+    title, author = book_data
+
+    @books << Book.new(title, author)
+    puts 'Book created successfully'
+  end
+
+  def book_data
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+
+    [title, author]
+  end
+end
